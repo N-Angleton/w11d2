2a4f04e30fcc2d333eece70a67dcf4966b9d17b2
App Academy Student <email@email.com>
2022-04-13 15:45:25 -0400

    finish phase 3

---
+++ frontend/components/todos/todo_form.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+
+
+export class TodoForm extends React.Component { 
+
+    constructor(props){
+        super(props);
+        this.state = {
+            id: new Date().getTime(),
+            title: '',
+            body: '',
+            done: null
+        };
+        this.updateDone = this.updateDone.bind(this);
+        this.updateTitle = this.updateTitle.bind(this);
+        this.updateBody = this.updateBody.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(e){
+        e.preventDefault();
+        this.props.receiveTodo(this.state);
+        this.setState({
+            id: new Date().getTime(),
+            title: '',
+            body: '',
+            done: null
+        });
+    }
+
+    updateDone(e){
+        this.setState({done: e.target.value});
+    }
+    
+    updateBody(e){
+        this.setState({ body: e.target.value });
+    }
+
+    updateTitle(e){
+        this.setState({ title: e.target.value });
+    }
+
+    render(){
+        return (
+            <form >
+                <label>Title: 
+                    <input type="text" onChange={this.updateTitle} value={this.state.title}/>
+                </label>
+                <br />
+                <label>Body: 
+                    <input type="text" onChange={this.updateBody} value={this.state.body}/>
+                </label>
+                <br/>
+                <label>Done:
+                    <label>True: 
+                        <input type="radio" onChange={this.updateDone} name="done" value="true" />
+                    </label>
+                    <label>False: 
+                        <input type="radio" name="done" value="false" defaultChecked/>
+                    </label>
+                </label>
+                <br />
+                <button onClick={this.handleClick}>Submit</button>
+            </form>
+        )
+    }
+
+}
--- frontend/components/todos/todo_list.jsx
+++ frontend/components/todos/todo_list.jsx
@@ -1,11 +1,14 @@
 import React from "react"; 
 import { TodoListItem } from "./todo_list_item";
-
+import { TodoForm } from "./todo_form";
 
 export function TodoList({receiveTodo, todos}) {
     return (
-        <ul>
-            {todos.map( todo => <TodoListItem todo={todo}/>)}
-        </ul>
+        <>
+            <ul>
+                {todos.map( todo => <TodoListItem key={todo.id} todo={todo}/>)}
+            </ul>
+            <TodoForm receiveTodo={receiveTodo}/>
+        </>
     )
 }
